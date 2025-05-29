66444e5311bd2d782b7a4ab4c495634d5be42d7d
WahiDestroyer <laamwahidtahosin@gmail.com>
2025-05-29 20:18:04 +0600

    animation

--- src/components/layout/Index.jsx
+++ src/components/layout/Index.jsx
@@ -7,7 +7,7 @@ import "aos/dist/aos.css";
 
 const Layout = () => {
   useEffect(() => {
-    Aos.init({ duration: 3000, once: true });
+    Aos.init({ duration: 2000, once: true });
   }, []);
   return (
     <>
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -3,6 +3,10 @@ import { Link } from "react-router";
 import BtnPrimary from "../Utility/BtnPrimary";
 
 const Navbar = () => {
+ 
+  
+  
+
   return (
     <nav data-aos="fade-down" className="py-5 bg-white">
       <div className="container flex items-center justify-between">
@@ -14,25 +18,37 @@ const Navbar = () => {
         <div className="flex items-center">
           <ul className="flex text-nav font-medium text-base">
             <li>
-                <Link to="/" className="py-3 px-6">Home</Link>
+              <Link to="/" className="py-3 px-6">
+                Home
+              </Link>
             </li>
             <li>
-                <Link to="/about" className="py-3 px-6">About</Link>
+              <Link to="/about" className="py-3 px-6">
+                About
+              </Link>
             </li>
             <li>
-                <Link to="/" className="py-3 px-6">Process</Link>
+              <Link to="/" className="py-3 px-6">
+                Process
+              </Link>
             </li>
             <li>
-                <Link to="/" className="py-3 px-6">Portfolio</Link>
+              <Link to="/" className="py-3 px-6">
+                Portfolio
+              </Link>
             </li>
             <li>
-                <Link to="/" className="py-3 px-6">Blog</Link>
+              <Link to="/" className="py-3 px-6">
+                Blog
+              </Link>
             </li>
             <li>
-                <Link to="/" className="py-3 px-6">Services</Link>
+              <Link to="/" className="py-3 px-6">
+                Services
+              </Link>
             </li>
           </ul>
-        <BtnPrimary path="/" title="Contact"/>
+          <BtnPrimary path="/" title="Contact" />
         </div>
       </div>
     </nav>
